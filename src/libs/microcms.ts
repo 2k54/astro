664b9178bd219e3a2973bbb93d256ca18365d8fb
2k54 <cusing335@gmail.com>
2023-09-22 11:17:11 +0900

    first commit

---
+++ src/libs/microcms.ts
@@ -0,0 +1,10 @@
+import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export const getBlogs = async (queries: MicroCMSQueries) => {
+  return await client.get({ endpoint: "test", queries });
+};
