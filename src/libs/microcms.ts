5ea26a6197b90cb9d89a91c2b7f9fd6c90452d4e
2k54 <cusing335@gmail.com>
2023-09-24 02:58:15 +0900

    test done

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,10 +1,21 @@
-import { createClient, type MicroCMSQueries } from "microcms-js-sdk";
+import { createClient, MicroCMSQueries } from "microcms-js-sdk";
 
 const client = createClient({
   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
   apiKey: import.meta.env.MICROCMS_API_KEY,
 });
 
-export const getBlogs = async (queries: MicroCMSQueries) => {
+export const getBlogs = async (queries?: MicroCMSQueries) => {
   return await client.get({ endpoint: "test", queries });
+};
+
+export const getBlogDetail = async (
+  blogId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail({
+    endpoint: "test",
+    contentId: blogId,
+    queries,
+  });
 };
